9cb18b308b8455557e047ee6f38c7df38e6b5725
Richard Fenix <richard.fenix@gmail.com>
2018-05-25 03:03:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1.0)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"  
 end
